44cfa8de2e2e803bea8e0a0e5dc15934ebc2700b
alan skovrlj <skovrljalan@gmail.com>
2022-03-19 12:43:41 +0100

    jnoviji manifest mora imat zadan root

--- PWA/sw.js
+++ PWA/sw.js
@@ -1,5 +1,5 @@
 let cacheName = "my-first-pwa";
-let filesToCache = ["/PWA/", "/PWA/index.html", "/PWA/css/style.css", "/PWA/js/main.js"];
+let filesToCache = ["/", "/index.html", "/css/style.css", "/js/main.js"];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener("install", (e) => {
